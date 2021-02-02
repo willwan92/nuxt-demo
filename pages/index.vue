5acfa1fabe081819b18e7a724d8249bed73f0e2f
willwan92 <wilson3cy@163.com>
2021-02-02 14:38:31 +0800

    初始化项目

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        nuxt-demo
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
