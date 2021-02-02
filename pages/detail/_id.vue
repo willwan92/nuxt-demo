4b33b0b56c3cd919f9ab06eaa70749b77ebe20ef
willwan92 <wilson3cy@163.com>
2021-02-02 15:19:39 +0800

    导航组件传参

---
+++ pages/detail/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>商品详情：{{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,38 @@
   <div class="container">
     <div>
       <h1 class="title">
-        nuxt-demo
+        商品列表
       </h1>
+      <ul>
+        <li v-for="item in goods" :key="item.id">
+          <nuxt-link :to="`/detail/${item.id}`">{{ item.name }}</nuxt-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      goods: [
+        {
+          id: 1,
+          name: '椰子'
+        },
+        {
+          id: 2,
+          name: '猕猴桃'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 </style>
